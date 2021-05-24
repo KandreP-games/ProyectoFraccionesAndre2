da2fd181f5c5710180ac2266d9db9dbcfeae6831
kleber24021 <kleber24021@gmail.com>
2021-05-24 22:18:35 +0200

    Commit funcion suma multiplicacion y division implementadas

--- src/clases/Fraccion.java
+++ src/clases/Fraccion.java
@@ -12,21 +12,27 @@ public class Fraccion {
 		this.numerador = numerador;
 		this.denominador = denominador;
 	}
-/*
- * 	Añadir las siguientes operaciones a Fracción
- * 
- * 
- * 
 	public static Fraccion simplifica(Fraccion entrada) {
+		return entrada;
 	}
 	
 	public static Fraccion suma(Fraccion sumando1, Fraccion sumando2) {
+		int denominadorComun = sumando1.getDenominador() * sumando2.getDenominador();
+
+		int numerador1 = sumando1.getNumerador() * sumando2.getDenominador();
+		int numerador2 = sumando2.getNumerador() * sumando1.getDenominador();
+
+		Fraccion suma = new Fraccion(numerador1 + numerador2, denominadorComun);
+
+		return simplifica(suma);
 	}
 	public static Fraccion multiplicacion(Fraccion sumando1, Fraccion sumando2) {
+		return simplifica(new Fraccion(sumando1.getNumerador() * sumando2.getDenominador(), sumando1.getDenominador() * sumando2.getNumerador()));
 	}
 	public static Fraccion division(Fraccion sumando1, Fraccion sumando2) {
+		return simplifica(new Fraccion(sumando1.getNumerador() * sumando2.getNumerador(), sumando1.getDenominador() * sumando2.getDenominador()));
 	}
-*/
+
 	public int getNumerador() {
 		return numerador;
 	}
@@ -46,9 +52,7 @@ public class Fraccion {
 
 	@Override
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(numerador).append(" / ").append(denominador);
-		return sb.toString();
+		return numerador + " / " + denominador;
 	}
 
 }
