74aa294005e3f34c821386c2206e2c8a597415d9
kleber24021 <kleber24021@gmail.com>
2021-05-24 22:17:03 +0200

    Commit funcion Simplificar implementada

--- src/clases/Fraccion.java
+++ src/clases/Fraccion.java
@@ -16,10 +16,27 @@ public class Fraccion {
  * 	Añadir las siguientes operaciones a Fracción
  * 
  * 
- * 
+ */
 	public static Fraccion simplifica(Fraccion entrada) {
+		int n1 = entrada.getNumerador();
+		int n2 = entrada.getDenominador();
+
+		int temp1 = n1;
+		int temp2 = n2;
+
+		while (n1 != n2){
+			if (n1 > n2){
+				n1 = n1 - n2;
+			}else {
+				n2 = n2 - n1;
+			}
+		}
+		int n3 = temp1 / n1;
+		int n4 = temp2 / n1;
+
+		return new Fraccion(n3, n4);
 	}
-	
+/*
 	public static Fraccion suma(Fraccion sumando1, Fraccion sumando2) {
 	}
 	public static Fraccion multiplicacion(Fraccion sumando1, Fraccion sumando2) {
@@ -46,9 +63,7 @@ public class Fraccion {
 
 	@Override
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(numerador).append(" / ").append(denominador);
-		return sb.toString();
+		return numerador + " / " + denominador;
 	}
 
 }
